f72b7c34e1860e43174f13c82c927b5b12958a69
eason <cheng.yi@capitalcrux.com>
2021-04-25 10:02:34 +0800

    UE

--- src/pages/BackConfig/BudgetPeriod/PeriodList/index.js
+++ src/pages/BackConfig/BudgetPeriod/PeriodList/index.js
@@ -3,7 +3,7 @@ import cls from 'classnames';
 import { get } from 'lodash';
 import { connect } from 'dva';
 import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
-import { Button, Card, Popconfirm, Drawer } from 'antd';
+import { Button, Card, Popconfirm, Drawer, Tag } from 'antd';
 import { ExtTable, BannerTitle, ExtIcon } from 'suid';
 import { FilterView } from '@/components';
 import { constants } from '@/utils';
@@ -20,6 +20,7 @@ class PeriodList extends Component {
     super(props);
     this.state = {
       selectedRowKeys: [],
+      delRowId: null,
     };
   }
 
@@ -41,6 +42,10 @@ class PeriodList extends Component {
   };
 
   edit = rowData => {
+    const disabled = this.actionDisabled(rowData);
+    if (disabled) {
+      return false;
+    }
     const { dispatch } = this.props;
     dispatch({
       type: 'budgetPeriod/updateState',
@@ -86,7 +91,7 @@ class PeriodList extends Component {
       },
       () => {
         dispatch({
-          type: 'budgetMaster/del',
+          type: 'budgetPeriod/del',
           payload: {
             id: record.id,
           },
@@ -107,7 +112,7 @@ class PeriodList extends Component {
     const { dispatch } = this.props;
     const { selectedRowKeys: ids } = this.state;
     dispatch({
-      type: 'budgetPeriod/del',
+      type: 'budgetPeriod/closePeriods',
       payload: ids,
       callback: res => {
         if (res.success) {
@@ -137,7 +142,9 @@ class PeriodList extends Component {
     if (loading.effects['budgetMaster/del'] && delRowId === row.id) {
       return <ExtIcon className="del-loading" type="loading" antd />;
     }
-    return <ExtIcon className="del" type="delete" antd />;
+    return (
+      <ExtIcon className={cls('del', { disabled: this.actionDisabled(row) })} type="delete" antd />
+    );
   };
 
   saveCustomizePeriod = data => {
@@ -205,6 +212,32 @@ class PeriodList extends Component {
     return null;
   };
 
+  renderPeriodName = (t, r) => {
+    if (r.closed) {
+      return (
+        <>
+          <span style={{ color: 'rgba(0,0,0,0.35)' }}>{t}</span>
+          <Tag style={{ marginLeft: 4, color: 'rgba(0,0,0,0.35)' }}>已关闭</Tag>
+        </>
+      );
+    }
+    return t;
+  };
+
+  renderDisabled = (t, r) => {
+    if (r.closed) {
+      return <span style={{ color: 'rgba(0,0,0,0.35)' }}>{t}</span>;
+    }
+    return t || '-';
+  };
+
+  actionDisabled = row => {
+    if (row.type !== PERIOD_TYPE.CUSTOMIZE.key) {
+      return true;
+    }
+    return false;
+  };
+
   render() {
     const { selectedRowKeys } = this.state;
     const hasSelected = selectedRowKeys.length > 0;
@@ -221,13 +254,19 @@ class PeriodList extends Component {
         required: true,
         render: (_text, record) => (
           <span className={cls('action-box')} onClick={e => e.stopPropagation()}>
-            <ExtIcon className="edit" onClick={() => this.edit(record)} type="edit" antd />
+            <ExtIcon
+              className={cls('edit', { disabled: this.actionDisabled(record) })}
+              onClick={() => this.edit(record)}
+              type="edit"
+              antd
+            />
             <Popconfirm
               placement="topLeft"
               title={formatMessage({
                 id: 'global.delete.confirm',
                 defaultMessage: '确定要删除吗？提示：删除后不可恢复',
               })}
+              disabled={this.actionDisabled(record)}
               onConfirm={() => this.del(record)}
             >
               {this.renderDelBtn(record)}
@@ -238,25 +277,20 @@ class PeriodList extends Component {
       {
         title: '期间名称',
         dataIndex: 'name',
-        width: 220,
-      },
-      {
-        title: '期间类型',
-        dataIndex: 'type',
-        width: 100,
-        render: t => t || '-',
+        width: 320,
+        render: (t, r) => this.renderPeriodName(t, r),
       },
       {
         title: '开始日期',
         dataIndex: 'startDate',
         width: 120,
-        render: t => t || '-',
+        render: (t, r) => this.renderDisabled(t, r),
       },
       {
         title: '结束日期',
         dataIndex: 'endDate',
         width: 120,
-        render: t => t || '-',
+        render: (t, r) => this.renderDisabled(t, r),
       },
     ];
     const closing = loading.effects['budgetPeriod/closePeriod'];
@@ -265,7 +299,7 @@ class PeriodList extends Component {
         <>
           <FilterView
             title="期间类型"
-            style={{ marginRight: 16, minWidth: 120 }}
+            style={{ marginRight: 16, minWidth: 140 }}
             currentViewType={selectPeriodType}
             viewTypeData={periodTypeData}
             onAction={this.handlerPeriodTypeChange}
