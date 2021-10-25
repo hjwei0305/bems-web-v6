cdbc95c5b8a358485441ca1a5d545273befa7923
eason <cheng.yi@capitalcrux.com>
2021-10-25 08:03:52 +0800

    UE

--- src/pages/BackConfig/BudgetStrategy/index.js
+++ src/pages/BackConfig/BudgetStrategy/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import cls from 'classnames';
-import { Button, Popconfirm } from 'antd';
-import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
-import { ExtTable, ExtIcon } from 'suid';
+import { Button } from 'antd';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+import { ExtTable } from 'suid';
 import { StrategyType } from '@/components';
 import { constants } from '@/utils';
-import FormModal from './FormModal';
 import styles from './index.less';
 
 const { SERVER_PATH } = constants;
@@ -15,135 +14,14 @@ const { SERVER_PATH } = constants;
 class BudgetStrategy extends Component {
   static tablRef;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      delRowId: null,
-    };
-  }
-
   reloadData = () => {
     if (this.tablRef) {
       this.tablRef.remoteDataRefresh();
     }
   };
 
-  add = () => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'budgetStrategy/updateState',
-      payload: {
-        showModal: true,
-        rowData: null,
-      },
-    });
-  };
-
-  edit = rowData => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'budgetStrategy/updateState',
-      payload: {
-        showModal: true,
-        rowData,
-      },
-    });
-  };
-
-  save = data => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'budgetStrategy/save',
-      payload: {
-        ...data,
-      },
-      callback: res => {
-        if (res.success) {
-          dispatch({
-            type: 'budgetStrategy/updateState',
-            payload: {
-              showModal: false,
-            },
-          });
-          this.reloadData();
-        }
-      },
-    });
-  };
-
-  del = record => {
-    const { dispatch } = this.props;
-    this.setState(
-      {
-        delRowId: record.id,
-      },
-      () => {
-        dispatch({
-          type: 'budgetStrategy/del',
-          payload: {
-            id: record.id,
-          },
-          callback: res => {
-            if (res.success) {
-              this.setState({
-                delRowId: null,
-              });
-              this.reloadData();
-            }
-          },
-        });
-      },
-    );
-  };
-
-  closeFormModal = () => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'budgetStrategy/updateState',
-      payload: {
-        showModal: false,
-        rowData: null,
-      },
-    });
-  };
-
-  renderDelBtn = row => {
-    const { loading } = this.props;
-    const { delRowId } = this.state;
-    if (loading.effects['budgetStrategy/del'] && delRowId === row.id) {
-      return <ExtIcon className="del-loading" type="loading" antd />;
-    }
-    return <ExtIcon className="del" type="delete" antd />;
-  };
-
   render() {
-    const { budgetStrategy, loading } = this.props;
-    const { showModal, rowData } = budgetStrategy;
     const columns = [
-      {
-        title: formatMessage({ id: 'global.operation', defaultMessage: '操作' }),
-        key: 'operation',
-        width: 100,
-        align: 'center',
-        dataIndex: 'id',
-        className: 'action',
-        required: true,
-        render: (text, record) => (
-          <span className={cls('action-box')}>
-            <ExtIcon className="edit" onClick={() => this.edit(record)} type="edit" antd />
-            <Popconfirm
-              placement="topLeft"
-              title={formatMessage({
-                id: 'global.delete.confirm',
-                defaultMessage: '确定要删除吗？提示：删除后不可恢复',
-              })}
-              onConfirm={() => this.del(record)}
-            >
-              {this.renderDelBtn(record)}
-            </Popconfirm>
-          </span>
-        ),
-      },
       {
         title: '策略名称',
         dataIndex: 'name',
@@ -164,19 +42,9 @@ class BudgetStrategy extends Component {
         render: t => t || '-',
       },
     ];
-    const formModalProps = {
-      save: this.save,
-      rowData,
-      showModal,
-      closeFormModal: this.closeFormModal,
-      saving: loading.effects['budgetStrategy/save'],
-    };
     const toolBarProps = {
       left: (
         <>
-          <Button type="primary" onClick={this.add}>
-            <FormattedMessage id="global.add" defaultMessage="新建" />
-          </Button>
           <Button onClick={this.reloadData}>
             <FormattedMessage id="global.refresh" defaultMessage="刷新" />
           </Button>
@@ -202,7 +70,6 @@ class BudgetStrategy extends Component {
     return (
       <div className={cls(styles['container-box'])}>
         <ExtTable {...tableProps} />
-        <FormModal {...formModalProps} />
       </div>
     );
   }
