59b30148107c4f7c40836e01bd004c3ad206e71c
eason <cheng.yi@capitalcrux.com>
2022-03-29 16:00:40 +0800

    UE

--- src/pages/BudgetOrder/Adjust/Order/Banner/index.js
+++ src/pages/BudgetOrder/Adjust/Order/Banner/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import cls from 'classnames';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import { BarCode } from 'suid';
 import { userUtils } from '@/utils';
 import RequestViewState from '../../../components/RequestViewState';
 import Action from './Action';
@@ -31,19 +30,16 @@ class Banner extends Component {
     return (
       <div className={cls(styles['banner-box'], 'horizontal')}>
         <div className="banner-content row-start horizontal">
-          <BarCode
-            text={get(headData, 'code') || 'No.-'}
-            textAlign="left"
-            height={42}
-            width={0.92}
-            wrapperClassName="bar-code"
-          />
           <div className="banner-detail vertical">
             <div className="title">
               {title}
               <span className="status">{this.renderStatus()}</span>
             </div>
             <div className="sub-title">
+              <span className="title-item">
+                <span className="label">单据号</span>
+                <span className="creator">{get(headData, 'code') || '-'}</span>
+              </span>
               <span className="title-item">
                 <span className="label">制单人</span>
                 <span className="creator">
--- src/pages/BudgetOrder/Injection/Order/Banner/index.js
+++ src/pages/BudgetOrder/Injection/Order/Banner/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import cls from 'classnames';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import { BarCode } from 'suid';
 import { userUtils } from '@/utils';
 import RequestViewState from '../../../components/RequestViewState';
 import Action from './Action';
@@ -37,19 +36,16 @@ class Banner extends Component {
     return (
       <div className={cls('suid-banner', styles['banner-box'], 'horizontal')}>
         <div className="banner-content row-start horizontal">
-          <BarCode
-            encodeText={get(headData, 'code') || 'No.-'}
-            textAlign="left"
-            height={42}
-            width={0.92}
-            fontSize={14}
-          />
           <div className="banner-detail vertical">
             <div className="title">
               {title}
               <span className="status">{this.renderStatus()}</span>
             </div>
             <div className="sub-title">
+              <span className="title-item">
+                <span className="label">单据号</span>
+                <span className="creator">{get(headData, 'code') || '-'}</span>
+              </span>
               <span className="title-item">
                 <span className="label">制单人</span>
                 <span className="creator">
